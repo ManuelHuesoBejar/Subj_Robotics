afa8df87023c38d2db97b541c9116ec1bb772e5a
Lucas-Go-Vel <lucas.gomez.velayos@alumnos.upm.es>
2023-04-19 12:35:26 +0200

    Update angles.hpp

--- src/program01/angles.hpp
+++ src/program01/angles.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
 template <typename N/*, typename = std::enable_if_t<std::is_base_of<int,N>::value>*/> N deg2rad(N);
-template <typename N> N rad2deg(N angle);
+template <typename N/*, static_assert(std::is_same<N, double>::value || std::is_same<N, float>::value)*/
+> N rad2deg(N angle);
