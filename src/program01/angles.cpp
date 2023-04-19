909ec966bc4b0a339d4b95f50911bbfe74e10620
Lucas-Go-Vel <lucas.gomez.velayos@alumnos.upm.es>
2023-04-19 16:48:42 +0200

    Preliminary angles functions finished
    -Wrapped math.h trigonometry functions for direct use with degrees

--- src/program01/angles.cpp
+++ src/program01/angles.cpp
@@ -1,8 +1,31 @@
 #include "angles.hpp"
+#include <math.h>
 
-template <typename N> N deg2rad(N angle){
+template <typename N> inline N deg2rad(N angle){
     return angle/180*PI;
 }
-template <typename N> N rad2deg(N angle){
+template <typename N> inline N rad2deg(N angle){
     return angle/PI*180;
+}
+template <typename N> inline double sin_deg(N angle){
+    return sin(deg2rad(angle));
+}
+template <typename N> inline double cos_deg(N angle){
+    return cos(deg2rad(angle));
+}
+template <typename N> inline double tan_deg(N angle){
+    return tan(deg2rad(angle));
+}
+template <typename N> inline double asin_deg(N angle){
+    return asin(deg2rad(angle));
+}
+template <typename N> inline double acos_deg(N angle){
+    return acos(deg2rad(angle));
+}
+template <typename N> inline double atan_deg(N angle){
+    return atan(deg2rad(angle));
+}
+//atan2 takes all 4 quadrants into account
+template <typename N> inline double atan2_deg(N angle){
+    return atan2(deg2rad(angle));
 }
--- src/program01/angles.hpp
+++ src/program01/angles.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
-template <typename N/*, typename = std::enable_if_t<std::is_base_of<int,N>::value>*/> N deg2rad(N);
-template <typename N/*, static_assert(std::is_same<N, double>::value || std::is_same<N, float>::value)*/
-> N rad2deg(N angle);
+template <typename N> inline N deg2rad(N);
+template <typename N> inline N rad2deg(N angle);
+//wraped math.h functions which work in radians, to work with degrees
+//in this case, since trigonometric functions return a double, I made it so x_deg functions also 
+//return a double instead of any type
+template <typename N> inline double sin_deg(N angle);
+template <typename N> inline double cos_deg(N angle);
+template <typename N> inline double tan_deg(N angle);
+template <typename N> inline double asin_deg(N angle);
+template <typename N> inline double acos_deg(N angle);
+template <typename N> inline double atan_deg(N angle);
+//atan2 takes all 4 quadrants into account
+template <typename N> inline double atan2_deg(N angle);
