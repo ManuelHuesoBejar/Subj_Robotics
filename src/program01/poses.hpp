a475a2ea24f1626157e5a5ad8dc4d3a4b6893c66
Lucas-Go-Vel <lucas.gomez.velayos@alumnos.upm.es>
2023-04-24 21:58:50 +0200

    Arreglo pose en base a feedback (WIP)

--- src/program01/poses.hpp
+++ src/program01/poses.hpp
@@ -2,10 +2,13 @@
 
 template <typename N>
 struct pose{
-    pose(N x=0, N y=0, N z=0, N _rx=0, N _ry=0, N _rz=0): px{x},py{y},pz{z},rx{_rx},ry{_ry},rz{_rz} {};
-    N px;
-    N py;
-    N pz;
+    pose(N t1=0, N l2=0, N z=0, N _rx=0, N _ry=0, N _rz=0): q1{t1},d2{l2},q3{z},rx{_rx},ry{_ry},rz{_rz} {};
+    //grado de la base
+    N q1;
+    //prismático
+    N d2;
+    //rotativo en prismática
+    N q3;
     N rx;
     N ry;
     N rz;
