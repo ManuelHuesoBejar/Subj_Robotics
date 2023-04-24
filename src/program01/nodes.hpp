ba14d0021ac401a45b8e719f5dfe1a76f079da5e
Lucas-Go-Vel <lucas.gomez.velayos@alumnos.upm.es>
2023-04-24 20:23:27 +0200

    Cambio en base a Feedback
    Seguro que está mal porque ahora mismo pose y node son lo mismo, pero @echedey-ls me dijo que pose contendría las coords de posición y rotación y node sería otra cosa. No he debido entender bien #5

--- src/program01/nodes.hpp
+++ src/program01/nodes.hpp
@@ -3,9 +3,11 @@
 
 template <typename N>
 struct node{
-    pose npose;
+    N px;
+    N py;
+    N pz;
     N rx;
     N ry;
     N rz;
-    node(N x=0, N y=0, N z=0, N _rx=0, N _ry=0, N _rz=0): npose{x,y,z},rx{_rx},ry{_ry},rz{_rz} {};
+    node(N x=0, N y=0, N z=0, N _rx=0, N _ry=0, N _rz=0): px{x},py{y},pz{z},rx{_rx},ry{_ry},rz{_rz} {};
 };
--- src/program01/poses.hpp
+++ src/program01/poses.hpp
@@ -2,8 +2,11 @@
 
 template <typename N>
 struct pose{
-    pose(N x=0, N y=0, N z=0): px{x}, py{y}, pz{z} {};
+    pose(N x=0, N y=0, N z=0, N _rx=0, N _ry=0, N _rz=0): px{x},py{y},pz{z},rx{_rx},ry{_ry},rz{_rz} {};
     N px;
     N py;
     N pz;
+    N rx;
+    N ry;
+    N rz;
 };
