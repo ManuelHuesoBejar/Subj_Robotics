8093216ac3e4d105deda47e92533f2aa81ac5592
Echedey Luis <80125792+echedey-ls@users.noreply.github.com>
2023-04-24 21:09:22 +0200

    Revert "Poses y Nodos (#6)"
    This reverts commit c4174171dbb3685d236f845c436bf3b848a82e39.

--- src/program01/program01.ino
+++ src/program01/program01.ino
@@ -8,62 +8,33 @@ File: program01.ino
 Abstract: primer prototipo de software para probar cosiñas
 */
 
-/* IntelliSense fix for VSCode, hardcoded, let it rest here for the team */
+// Lo siento, uso VSCode
 #include "Arduino.h"
 
-/* Inclusión de cabeceras del programa */
+// Inclusión de cabeceras del programa
 #include "rotation_sensor.hpp"
 
-/* Instrucciones para la manipulación directa de registros */
-// defines for setting and clearing register bits
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
-
-/* Características modificables */
-// Aumentar frecuencia de la señal de reloj del ADC
-// [1] https://forum.arduino.cc/t/faster-analog-read/6604/6
-#define FASTADC 1
-
-/* Pines de los sensores */
+// Pines de los sensores
 #define SENS_Q1 A0
 
-/* Estados disponibles del robot */
-static enum State_machine {
+// Estados disponibles del robot
+enum State_machine {
     uninitialized_robot = 0,
     emergency_stop = 1,
     homing_required,
     swabbing_available,
     swabbing_procedure,
-    swab_pick_up,
-    swab_place,
+    swab_change_required,
 } state;
 
-static Rotation_sensor Q1_sens{SENS_Q1, 3600.0};
+Rotation_sensor Q1_sens{SENS_Q1, 3600.0};
 
 void setup() {
     state = uninitialized_robot;
     Q1_sens.begin();
-
-#if FASTADC // [1], faster ADC
-    // set prescale to 16
-    sbi(ADCSRA,ADPS2);
-    cbi(ADCSRA,ADPS1);
-    cbi(ADCSRA,ADPS0);
-#endif
-    Serial.begin(115200);
-    Serial.println(">> Webo!");
-    Serial.println("Sys: Modelo M ha despertado");
-    Serial.println(">> Hola, me dispongo a meterte un hisopo por la nariz.");
-    Serial.println("Sys: Modelo M te está amenazando, hazle caso.");
 }
 
 void loop() {
-    Serial.print(Q1_sens.read()); Serial.print(" --> ");
-    Serial.print(Q1_sens.read_angle()); Serial.println("º");
     switch (state)
     {
     case uninitialized_robot:
@@ -76,9 +47,7 @@ void loop() {
         break;
     case swabbing_procedure:
         break;
-    case swab_pick_up:
-        break;
-    case swab_place:
+    case swab_change_required:
         break;
 
     default:
