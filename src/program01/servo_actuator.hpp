be96451a34655a1c123325ce706032b744e85cd8
Lucas-Go-Vel <lucas.gomez.velayos@alumnos.upm.es>
2023-05-05 19:39:27 +0200

    Servo_act update
    -update based on feedback
    -not finished

--- src/program01/servo_actuator.hpp
+++ src/program01/servo_actuator.hpp
@@ -1,24 +1,33 @@
 #pragma once
 #include "actuator.hpp"
+#include "rotation_sensor.hpp"
 #include <Servo.h>
 
+int deg2microseconds(double grados){return map(grados, 10, 170, 700, 2300);}
+double microsecond2degs(int microseconds){return map(microseconds, 700, 2300, 10, 170);}
+
 class Servo_Feedback: public Actuator{
-    int pin_feedback;
-    int pin;
+    //int pin_feedback;
+    Rotation_sensor pote;
+    uint8_t pin_s;
     public:
-    Servo_Feedback():Actuator(){
-      this->set_limits(0, 180);
+    Servo_Feedback(uint8_t pin_signal, uint8_t pin_pote, double spin_range_, double i_rel_ = 1.0):Actuator(), Rotation_sensor(pin_pote, spin_range_, i_rel_), 
+    pin_s{pin_signal}{
       Servo myservo;
-      myservo.attach(pin);  // attaches the servo on pin 9 to the servo object
+      myservo.attach(pin);
     }
-    virtual ~Servo_Feedback(){delete myservo;}
-    void get_current_pos(){
+    virtual ~Servo_Feedback(){myservo.detach(); delete myservo;}
+    
+    void begin() {pinMode(pin, OUTPUT); pinMode(pote.pin, INPUT);}
+    
+    double get_current_pos(){
       int pos = analogRead(pin_feedback);
-      pos = map(pos, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
-      current_pos = (double)pos;
+      current_pos = microsecond2degs(pos); 
+      //puede que sea mejor
+      current_pos = pote.read_angle();
     }
     int move(){
-        myservo.write(target_pos);
-        if (expected_pos!=current_pos) return ID;
+        myservo.writeMicroseconds(deg2microseconds(target_pos));
+        //return ID;
     }
 };
