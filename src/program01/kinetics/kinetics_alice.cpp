2f5bc0ad12dcfc8119799850a736e49a3aae6968
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2023-05-09 15:00:59 +0200

    Estructurar archivo cinemáticas

---
+++ src/program01/kinetics/kinetics_alice.cpp
@@ -0,0 +1,20 @@
+#include "nodes.hpp"
+#include "poses.hpp"
+
+int alice_direct(const Pose& in, Node& out) {
+    /** @brief Calcula las coordenadas del efector de Alice
+     * @arg in : coordenadas articulares
+     * @arg out : coordenadas espaciales
+     * @returns int : 0 si no hay error, no-nulo si lo hay
+    */
+}
+
+int alice_inverse(const Node& in, const Pose& current, Pose& out) {
+    /** @brief Calcula las coordenadas articulares para determinada posición del efector
+     * @details Devuelve las coordenadas más próximas a la actual, siempre que sea alcanzable
+     * @arg in : coordenadas espaciales del efector
+     * @arg current : coordenadas actuales de Alice (el robot)
+     * @arg out : coordenadas articulares correspondientes
+     * @returns int : 0 si no hay error, no-nulo si lo hay
+    */
+}
