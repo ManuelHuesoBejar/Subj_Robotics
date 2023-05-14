5bec9dfa3888f383ecef63112d4aafdde978ce3c
DRP <diego.rpeinado@alumnos.upm.es>
2023-05-14 14:23:08 +0200

    Añadidos limitadores para la cinemática
    Faltan algunas condiciones para limitar los angulos de los actuadores

--- src/program01/alice.cpp
+++ src/program01/alice.cpp
@@ -37,6 +37,17 @@ Alice::direct(const Pose& in, Node& out) {
     out.ry = in.q4 + in.q3;
     out.rz = 0;
 
+    //Comprobamos que la posición final se encuentre dentro de los límites del espacio de trabajo del robot
+    //Colocamos el origen de coordenadas en la base. Análisis del peor caso:
+    //Añadir una condicion en out.rz para que no se supere el giro maximo del servo?
+    if ((out.px > (l2 / 2 + l4 + l5))||(out.px < -(l2 / 2 + l4 + l5))||
+        (out.py > (l2 / 2 + l4 + l5))||(out.py < -(l2 / 2 + l4 + l5))||
+        (out.pz > (l1 + l4 + l5))    ||(out.pz < (l1 - l4 - l5)))
+    {
+        return 1;
+    }
+
+    else return 0;
 }
 
 int
@@ -50,9 +61,16 @@ Alice::inverse(const Node& in, Pose& out) {
     double x4 = l4 * cos(alpha);
     double d = in.px - (x5 + x4);
 
-    //Asignamos los valores obtenidos en la pose de salida 
+    //Hay que añadirle condiciones para limitar las rotaciones de los servos
+    if ((d > l2 / 2 || d < l2 / 2))
+    {
+        return 1
+    }
+
+    else{
     out.d2 = d;
     out.q3 = deg2rad(alpha);
     out.q4 = deg2rad(beta);
-
+    return 0;
+    }
 }
